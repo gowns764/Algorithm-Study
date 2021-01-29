1314a71318f79f0382353dc89f2f0a168e323ea3
Raspbian <gowns0719@gmail.com>
2021-01-30 02:32:24 +0900

    second commit

--- 파이썬 코드/BigO_Test.py
+++ 파이썬 코드/BigO_Test.py
@@ -1,7 +1,29 @@
-array = [3, 5, 1, 2, 4]
-summary = 0
+from random import randint
+import time
 
-for x in array:
-    summary += x
+array = []
+for _ in range(10000):
+    array.append(randint(1, 100))
 
-print(summary)
+start_time = time.time()
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
+
+end_time = time.time()
+print("선택 정렬 성능 측정 :", end_time - start_time)
+
+array = []
+for _ in range(10000):
+    array.append(randint(1, 100))
+
+start_time = time.time()
+
+array.sort()
+
+end_time = time.time()
+print("기본 정렬 라이브러리 성능 측정 :", end_time - start_time)
